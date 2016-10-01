7bc07f2b46140cad197dd88f5e9b63ede3a91df5
is161518 <is161518@fhstp.ac.at>
2016-10-01 14:43:07 +0200

    buttons edited - func

--- app/src/main/java/local/is161518_is161523/mycalculator/MainActivity.java
+++ app/src/main/java/local/is161518_is161523/mycalculator/MainActivity.java
@@ -5,10 +5,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String TAG = "MyCALC";
+    private short option_selected = 0;
+    private long last_value = 0;
+    /*
+    0=nothing
+    1=div
+    2=mul
+    3=min
+    4=plu
+    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -124,62 +135,173 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     }
 
     public void btn_0(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 0");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("0");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("0");
+        }
+
 
     }
     public void btn_1(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 1");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("1");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("1");
+        }
+
 
     }
     public void btn_2(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 2");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("2");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("2");
+        }
 
     }
     public void btn_3(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 3");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("3");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("3");
+        }
 
     }
     public void btn_4(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 4");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("4");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("4");
+        }
 
     }
     public void btn_5(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 5");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("5");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("5");
+        }
 
     }
     public void btn_6(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 6");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("6");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("6");
+        }
 
     }
     public void btn_7(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 7");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("7");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("7");
+        }
 
     }
     public void btn_8(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 8");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("8");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("8");
+        }
 
     }
     public void btn_9(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button 9");
+
+        if(option_selected != 0) {
+            last_value=Long.parseLong(tv_sum.getText().toString());
+            tv_sum.setText("9");
+            option_selected=0;
+        }
+        else{
+            tv_sum.append("9");
+        }
+
+    }
+
+    public void btn_div(View view) {
+        Log.i(TAG,"button div");
+        option_selected=1;
 
     }
     public void btn_mul(View view) {
         Log.i(TAG,"button mul");
-
-    }
-    public void btn_div(View view) {
-        Log.i(TAG,"button div");
-
-    }
-    public void btn_plu(View view) {
-        Log.i(TAG,"button plu");
+        option_selected=2;
 
     }
     public void btn_min(View view) {
         Log.i(TAG,"button min");
+        option_selected=3;
+
+    }
+    public void btn_plu(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
+        Log.i(TAG,"button plu");
+        option_selected=4;
+
 
     }
     public void btn_c(View view) {
@@ -187,7 +309,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void btn_sum(View view) {
+        TextView tv_sum = (TextView) findViewById(R.id.tv_sum);
         Log.i(TAG,"button sum");
+        long temp=0;
+        if(option_selected==4) {
+            temp = last_value + Long.parseLong(tv_sum.getText().toString());
+        }
+        tv_sum.setText(Long.toString(temp));
+
 
     }
 
